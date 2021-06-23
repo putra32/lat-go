47ca7682ab6192e4d8bc05fabb0399089d9bc030
putra32 <putralubis1432@gmail.com>
2021-06-23 09:40:17 +0700

    update commit

--- web-go/main.go
+++ web-go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path"
+	"text/template"
 )
 
 func handlerIndex(rw http.ResponseWriter, r *http.Request) {
@@ -18,9 +20,34 @@ func handlerHello(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
+
+	// Static router
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+
+	// Tempalte router
+	http.HandleFunc("/view", func(rw http.ResponseWriter, r *http.Request) {
+		var filepath = path.Join("views", "index.html")
+		var tmpl, err = template.ParseFiles(filepath)
+
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var data = map[string]interface{}{
+			"title": "Learning Golang Web",
+			"name":  "Batman",
+		}
+
+		err = tmpl.Execute(rw, data)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
 	var address = "localhost:9000"
 
